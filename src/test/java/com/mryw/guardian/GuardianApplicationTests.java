7b58ebb3c5b92ef777105f075e883a215672bd7c
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2022-03-27 11:51:14 +0200

    Model

---
+++ src/test/java/com/mryw/guardian/GuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mryw.guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
